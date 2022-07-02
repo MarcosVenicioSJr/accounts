899514823bd533bc1c31d76fc772865bee7edb04
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-07-02 10:59:35 -0300

    Criacao da funcao operation, criando uma lista para o usuario.

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const fs = require('fs')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 
+operation()
+
+function operation() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'accounts',
+        message: 'Selecione a opção desejada',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    },
+    ]).then()
+    .catch(err => console.log(err))
+}
